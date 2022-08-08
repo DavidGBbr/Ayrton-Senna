354400977328c3f6dbe36f2a832d705600e1db96
DavidGBbr <davidgomesbrigido@gmail.com>
2022-08-08 11:02:25 -0300

    styling tabs and title bar

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 import TelaInicio from './componentes/TelaInicio/';
 import TelaSobre from './componentes/TelaSobre/';
@@ -11,10 +14,56 @@ const Tabs = createBottomTabNavigator();
 export default function App() {
  return (
    <NavigationContainer>
-    <Tabs.Navigator>
-      <Tabs.Screen name='Início' component={TelaInicio}/>
-      <Tabs.Screen name='Sobre' component={TelaSobre}/>
-      <Tabs.Screen name='Vitórias' component={TelaVitorias}/>
+    <Tabs.Navigator
+      screenOptions={{
+        headerStyle:{
+          backgroundColor:'#eecb01',
+        },
+        headerTitleStyle:{
+          color:'#000',
+          fontWeight:'bold',
+        },
+
+        tabBarStyle:{
+          backgroundColor:'#eecb01',
+        },
+        tabBarLabelStyle:{
+          fontSize:16,
+        },
+        tabBarActiveTintColor:'#ba171',
+        tabBarInactiveTintColor:'#000',
+        tabBarActiveBackgroundColor:'#fff',
+      }}>
+
+      <Tabs.Screen 
+        name='Início' 
+        component={TelaInicio}
+        options={{
+          tabBarIcon:({color})=>(
+            <Entypo name="home" size={20} color={color}/>
+          )
+        }}
+        />
+
+      <Tabs.Screen 
+        name='Sobre' 
+        component={TelaSobre}
+        options={{
+          tabBarIcon:({color})=>(
+            <FontAwesome5 name="question-circle" size={20} color={color}/>
+          )
+        }}
+        />
+
+      <Tabs.Screen 
+        name='Vitórias' 
+        component={TelaVitorias}
+        options={{
+          tabBarIcon:({color})=>(
+            <FontAwesome5 name="trophy" size={20} color={color}/>
+          )
+        }}/>
+
     </Tabs.Navigator>
    </NavigationContainer>
   );
